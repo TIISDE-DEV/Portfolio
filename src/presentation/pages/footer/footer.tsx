00ae60c491b1fa1c29ba39a14abd0e3989d595ff
alexsouza <alexsouza160298@gmail.com>
2024-05-07 11:14:03 -0300

    fix: add icons to footer

--- src/presentation/pages/footer/footer.tsx
+++ src/presentation/pages/footer/footer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Connected, StyledInput, StyledButton, InputButton, LogoContainer, Logo, FooterText, CopyRightContainer, FooterTextContainer, SecondTextFooter, ButtonContainer, Button } from './footer-styled';
+import { Container, Connected, StyledInput, StyledButton, StyledImage, InputButton, LogoContainer, Logo, FooterText, CopyRightContainer, FooterTextContainer, SecondTextFooter, ButtonContainer, Button, arrow, twitterIcon, youtubeIcon, tiktokIcon, instagram } from './footer-styled';
 
 const Footer: React.FC = () => {
   const handleClick = () => {
@@ -11,16 +11,17 @@ const Footer: React.FC = () => {
       <InputButton>
         <Connected>Share your email for exclusive updates!</Connected>
         <StyledInput type="text" placeholder="Enter your email" />
-        <StyledButton onClick={handleClick}>Send</StyledButton>
+        <StyledButton onClick={handleClick}><StyledImage src={arrow} alt="arrow" /></StyledButton>
       </InputButton>
       <hr />
       <LogoContainer>
         <Logo>TIISDE</Logo>
-        <SecondTextFooter>At TIISDE, we specialize in transforming concepts into captivating experiences. Our adept team of developers pioneers innovative solutions tailored to your business, ensuring unparalleled quality and user satisfaction.
+        <SecondTextFooter>We specialize in transforming concepts into captivating experiences. Our adept team of developers pioneers innovative solutions tailored to your business, ensuring unparalleled quality and user satisfaction.
           <ButtonContainer>
-            <Button onClick={handleClick}>Icon</Button>
-            <Button onClick={handleClick}>Icon</Button>
-            <Button onClick={handleClick}>Icon</Button>
+            <Button onClick={handleClick}><StyledImage src={twitterIcon} alt="Twitter" /></Button>
+            <Button onClick={handleClick}><StyledImage src={youtubeIcon} alt="Youtube" /></Button>
+            <Button onClick={handleClick}><StyledImage src={tiktokIcon} alt="TikTok" /></Button>
+            <Button onClick={handleClick}><StyledImage src={instagram} alt="Instagram" /></Button>
           </ButtonContainer>
         </SecondTextFooter>
       </LogoContainer>
