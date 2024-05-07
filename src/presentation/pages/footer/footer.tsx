d59f833539df1792501095f3f77570b67fed4302
alexsouza <alexsouza160298@gmail.com>
2024-05-07 16:03:40 -0300

    fix: add buttons to footer

--- src/presentation/pages/footer/footer.tsx
+++ src/presentation/pages/footer/footer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Connected, StyledInput, StyledButton, StyledImage, InputButton, LogoContainer, Logo, FooterText, CopyRightContainer, FooterTextContainer, SecondTextFooter, ButtonContainer, Button, arrow, twitterIcon, youtubeIcon, tiktokIcon, instagram } from './footer-styled';
+import { Container, Connected, StyledInput, StyledButton, StyledImage, InputButton, LogoContainer, Logo, CopyRightContainer, FooterTextContainer, SecondTextFooter, ButtonContainer, Button, TermsOfServiceLink, PrivacyPolicyLink, FooterText } from './footer-styled';
 
 const Footer: React.FC = () => {
   const handleClick = () => {
@@ -11,17 +11,17 @@ const Footer: React.FC = () => {
       <InputButton>
         <Connected>Share your email for exclusive updates!</Connected>
         <StyledInput type="text" placeholder="Enter your email" />
-        <StyledButton onClick={handleClick}><StyledImage src={arrow} alt="arrow" /></StyledButton>
+        <StyledButton onClick={handleClick}>GO</StyledButton>
       </InputButton>
       <hr />
       <LogoContainer>
         <Logo>TIISDE</Logo>
         <SecondTextFooter>We specialize in transforming concepts into captivating experiences. Our adept team of developers pioneers innovative solutions tailored to your business, ensuring unparalleled quality and user satisfaction.
           <ButtonContainer>
-            <Button onClick={handleClick}><StyledImage src={twitterIcon} alt="Twitter" /></Button>
-            <Button onClick={handleClick}><StyledImage src={youtubeIcon} alt="Youtube" /></Button>
-            <Button onClick={handleClick}><StyledImage src={tiktokIcon} alt="TikTok" /></Button>
-            <Button onClick={handleClick}><StyledImage src={instagram} alt="Instagram" /></Button>
+            <Button onClick={handleClick}>Twitter</Button>
+            <Button onClick={handleClick}>Youtube</Button>
+            <Button onClick={handleClick}>TikTok</Button>
+            <Button onClick={handleClick}>Instagram</Button>
           </ButtonContainer>
         </SecondTextFooter>
       </LogoContainer>
@@ -29,8 +29,8 @@ const Footer: React.FC = () => {
       <CopyRightContainer>
         <FooterText>© 2024 All Rights Reserved by TIISDE</FooterText>
         <FooterTextContainer>
-          <FooterText>Terms Of Service</FooterText>
-          <FooterText>Privacy Policy</FooterText>
+          <TermsOfServiceLink href="#">Terms Of Service</TermsOfServiceLink>
+          <PrivacyPolicyLink href="#">Privacy Policy</PrivacyPolicyLink>
         </FooterTextContainer>
       </CopyRightContainer>
     </Container>
