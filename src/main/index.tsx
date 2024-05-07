7c3abb96a387f3ee108da349609dce05d60bbc72
alexsouza <alexsouza160298@gmail.com>
2024-05-07 11:08:59 -0300

    fix: add footer to index

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,4 +1,5 @@
 import { theme } from '@/context/theme'
+import { Footer } from '@/presentation/pages/footer/footer'
 import Login from '@/presentation/pages/login/login'
 import React from 'react'
 import ReactDOM from 'react-dom'
@@ -8,6 +9,7 @@ import { ThemeProvider } from 'styled-components'
 ReactDOM.render(
   <ThemeProvider theme={ theme }>
     <Login />
+    <Footer/>
   </ThemeProvider>,
   document.getElementById('main')
 )
